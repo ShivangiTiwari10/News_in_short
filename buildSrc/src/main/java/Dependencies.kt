2b5e4ba22ae9c6d589ddcab4e7c8fd7c71e70d62
Shivangi <shivangit1111@gmail.com>
2024-09-26 18:47:14 +0530

    build: create variable for dependency

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,4 +1,18 @@
 object Dependencies {
 
     val coreKtx by lazy { "androidx.core:core-ktx:${Versions.coreKtx}" }
+    val lifecycle by lazy { "androidx.lifecycle:lifecycle-runtime-ktx:${Versions.lifecycle}" }
+    val activityCompose by lazy { "androidx.activity:activity-compose:${Versions.activityCompose}" }
+    val composeUi by lazy { "androidx.compose.ui:ui" }
+    val composeBom by lazy { "androidx.compose:compose-bom:2023.08.00" }
+    val composeUiGraphics by lazy { "androidx.compose.ui:ui-graphics" }
+    val composeUiTooling by lazy { "androidx.compose.ui:ui-tooling-preview" }
+    val composeMaterial by lazy { "androidx.compose.material3:material3" }
+    val jUnit by lazy { "junit:junit:4.13.2" }
+    val testjUnit by lazy { "androidx.test.ext:junit:1.2.1" }
+    val testExpresso by lazy { "androidx.test.espresso:espresso-core:3.6.1"}
+    val testManifest by lazy{"androidx.compose.ui:ui-test-manifest"}
+    val debugTooling by lazy { "androidx.compose.ui:ui-tooling" }
+    val testJUnit by lazy { "androidx.compose.ui:ui-test-junit4" }
+    val testBom by lazy { "androidx.compose:compose-bom:2023.08.00" }
 }
--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -1,4 +1,6 @@
 object Versions {
 
     const val coreKtx = "1.13.1"
+    const val lifecycle ="2.8.6"
+    const val activityCompose ="1.9.2"
 }
