82d7cb4eef66429d41fbaa020393032067ff6fab
Shivangi <shivangit1111@gmail.com>
2024-09-28 15:39:58 +0530

    fix: @HiltViewModel annotated class should contain one @Inject annotated constructor

--- app/src/main/java/com/example/newsinshort/ui/viewmodel/NewsViewModel.kt
+++ app/src/main/java/com/example/newsinshort/ui/viewmodel/NewsViewModel.kt
@@ -1,8 +1,19 @@
 package com.example.newsinshort.ui.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class NewsViewModel: ViewModel() {
+class NewsViewModel @Inject constructor(): ViewModel() {
+    init {
+        Log.d(TAG,"init block of newsViewModel")
+
+    }
+
+
+    companion object {
+        const val TAG = "NewsViewModel"
+    }
 }
