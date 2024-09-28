0c2fd50e6568f50597d89dcdebc17fb0b3e976a5
Shivangi <shivangit1111@gmail.com>
2024-09-28 15:27:53 +0530

    feat: define Hiltviewmodel

---
+++ app/src/main/java/com/example/newsinshort/ui/viewmodel/NewsViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.newsinshort.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class NewsViewModel: ViewModel() {
+}
