5bbbe9f40b21ef787333c89a99d048152e21df4c
Shivangi <shivangit1111@gmail.com>
2024-09-29 13:42:33 +0530

    refactor: make function suspended

--- app/src/main/java/com/example/newsinshort/ui/data/datasource/NewsDataSourceImpl.kt
+++ app/src/main/java/com/example/newsinshort/ui/data/datasource/NewsDataSourceImpl.kt
@@ -6,7 +6,7 @@ import retrofit2.Response
 import javax.inject.Inject
 
 class NewsDataSourceImpl @Inject constructor(private val apiService: ApiService) : NewsDataSource {
-    override fun getNewsHeadline(country: String): Response<NewsResponse> {
+    override suspend fun getNewsHeadline(country: String): Response<NewsResponse> {
 
         return apiService.getNewsHeadline(country)
     }
