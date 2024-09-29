ad3547dc6e78ba2c605d4556bb37f7b6758d98cb
Shivangi <shivangit1111@gmail.com>
2024-09-29 15:13:20 +0530

    feat: create repository with dependencies

--- app/src/main/java/com/example/newsinshort/ui/repository/NewsRepository.kt
+++ app/src/main/java/com/example/newsinshort/ui/repository/NewsRepository.kt
@@ -1,5 +1,15 @@
 package com.example.newsinshort.ui.repository
 
-class NewsRepository {
+import com.example.newsinshort.ui.data.datasource.NewsDataSource
+import com.example.newsinshort.ui.data.entity.NewsResponse
+import retrofit2.Response
+import javax.inject.Inject
+
+class NewsRepository @Inject constructor(private val newsDataSource: NewsDataSource) {
+
+    suspend fun getNewsHeadline(country: String): Response<NewsResponse> {
+
+        return newsDataSource.getNewsHeadline(country)
+    }
 
 }
