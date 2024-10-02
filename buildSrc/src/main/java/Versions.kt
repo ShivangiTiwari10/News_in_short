db6a3e52dc9b2d4319e3e8391bd8da8e66f3ff0d
Shivangi <shivangit1111@gmail.com>
2024-10-02 17:56:26 +0530

    fix: error at(Ljava/lang/Object;I)Landroidx/compose/animation/core/KeyframesSpec$KeyframeEntity

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -7,7 +7,7 @@ object Versions {
     const val material ="1.12.0"
     const val testExpresso ="3.6.1"
     const val testJunit ="1.2.1"
-    const val composeBom ="2023.08.00"
+    const val composeBom ="2024.02.00"
     const val testBom ="2023.08.00"
     const val jUnit ="4.13.2"
     const val hilt ="2.51.1"
