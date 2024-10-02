dea98c5ac1e84c2555fbcd081f16ced86f69f6a1
Shivangi <shivangit1111@gmail.com>
2024-10-02 12:01:01 +0530

    feat: launch coroutine

--- app/src/main/java/com/example/newsinshort/ui/viewmodel/NewsViewModel.kt
+++ app/src/main/java/com/example/newsinshort/ui/viewmodel/NewsViewModel.kt
@@ -2,8 +2,11 @@ package com.example.newsinshort.ui.viewmodel
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.newsinshort.ui.repository.NewsRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -11,6 +14,15 @@ class NewsViewModel @Inject constructor(
     private val newsRepository: NewsRepository
 
 ) : ViewModel() {
+
+    suspend fun getNews(country: String) {
+
+        viewModelScope.launch(Dispatchers.IO) {
+            newsRepository.getNewsHeadline(country)
+        }
+    }
+
+
     init {
         Log.d(TAG, "init block of newsViewModel")
 
