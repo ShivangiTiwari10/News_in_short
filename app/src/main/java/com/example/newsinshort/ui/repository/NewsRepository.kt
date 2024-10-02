8c14698343dbfa9478098c4bdf5cd019ed3080b2
Shivangi <shivangit1111@gmail.com>
2024-10-02 11:38:11 +0530

    feat: create flow for api response

--- app/src/main/java/com/example/newsinshort/ui/repository/NewsRepository.kt
+++ app/src/main/java/com/example/newsinshort/ui/repository/NewsRepository.kt
@@ -2,14 +2,35 @@ package com.example.newsinshort.ui.repository
 
 import com.example.newsinshort.ui.data.datasource.NewsDataSource
 import com.example.newsinshort.ui.data.entity.NewsResponse
-import retrofit2.Response
+import com.example.utilities.ResourceState
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+import java.lang.Error
 import javax.inject.Inject
 
 class NewsRepository @Inject constructor(private val newsDataSource: NewsDataSource) {
 
-    suspend fun getNewsHeadline(country: String): Response<NewsResponse> {
+//    suspend fun getNewsHeadline(country: String): Response<NewsResponse> {
+//
+//        return newsDataSource.getNewsHeadline(country)
+//    }
 
-        return newsDataSource.getNewsHeadline(country)
+    suspend fun getNewsHeadline(country: String): Flow<ResourceState<NewsResponse>> {
+        return flow {
+            emit(ResourceState.Loading())
+            val response = newsDataSource.getNewsHeadline(country)
+
+            if (response.isSuccessful && response.body() != null) {
+                emit(ResourceState.Success(response.body()!!))
+            } else {
+
+                error(Error("Error Fetching News Data"))
+            }
+        }
+            .catch { e ->
+                emit(ResourceState.Error(e?.localizedMessage ?: "Some error in flow"))
+            }
     }
 
 }
--- utilities/src/main/java/com/example/utilities/ResourceState.kt
+++ utilities/src/main/java/com/example/utilities/ResourceState.kt
@@ -5,6 +5,6 @@ sealed class ResourceState<T> {
     class Loading<T> : ResourceState<T>()
     data class Success<T>(val data: T) : ResourceState<T>()
 
-    data class  Error<T> (val error :T) :ResourceState<T>()
+        data class Error<T>(val error: Any) : ResourceState<T>()
 
 }
